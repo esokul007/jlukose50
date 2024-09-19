6aba311ac50646d0c7a60175e6e1596fa450642a
esokul007 <esokul007@gmail.com>
2024-09-19 10:27:07 -0400

    workshop 5(edited)

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,9 +1,12 @@
-# Tawab Berri + Jack Blair + Jlukose
+# Tawab Berri
 # The Duo-lingos
 # SoftDev
 # K05 - Review on dictionaries and accessing files, as well as splitting strings in Python.
 # 2024-09-18
 # time spent: 0:20
+
+# COMMENT: This code does NOT work as intended in the following circumstances:
+# '$$$' or '@@@' found in ducky or devo name
 
 import random
 f = open("krewes.txt", "r")
@@ -18,8 +21,8 @@ while i < len(devos):
     i+=1
     
 print(dict)
-n = random.randint(0, len(dict) -1)
-print(n)
+n = random.randint(0, len(dict) -2) #extraneous dictionary entry for "\n" new line character at the end
+#print(n)
 
 devoinfo = dict[n].split("$$$")
 print("Period: " + devoinfo[0] + ", Name: " + devoinfo[1] + ", Ducky: " + devoinfo[2])
