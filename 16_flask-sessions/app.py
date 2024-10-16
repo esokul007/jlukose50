5e5bf09b8e56a45a991104f2ce307f639ffec722
esokul007 <esokul007@gmail.com>
2024-10-16 16:38:25 -0700

    Code I missed in class today

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -3,77 +3,44 @@ Tawab Berri
 The Duo-lingos - Tawab Berri, Jacob Lukose, Jack Blair
 SoftDev
 2024-10-08
-K15 - Analyzing the different forms of server responses of user inputs.
-time spent: 2.4 hrs
+K16 - Analyzing the different forms of server responses of user inputs.
+time spent: 2.9 hrs
 '''
-# import conventions:
-# list most general first (standard python library)
-# ...then pip installs (eg Flask)
-# ...then your own home-rolled modules/packages (today's test module)
 
-from flask import Flask             #facilitate flask webserving
-from flask import render_template   #facilitate jinja templating
-from flask import request           #facilitate form submission
-from flask import session as S
+# cited and adapted from victor casado's code
 
-import testmod0
-import secrets
+from flask import Flask, render_template, request, session, redirect
 import os
 
-#the conventional way:
-#from flask import Flask, render_template, request
+app = Flask(__name__)
+app.secret_key = os.urandom(32)
 
-app = Flask(__name__)    #create Flask object
-app.secret_key = secrets.token_hex()#"blah blah blah" #os.urandom(32)
-
-
-@app.route("/", methods=['GET', 'POST'])
-def disp_loginpage():
-    #print("one\n\n\n")
-    #print("***DIAG: this Flask obj ***")
-    #print(app)
-    #print("***DIAG: request obj ***")
-    #print(request)
-    #print("***DIAG: request.args ***")
-    #print(request.args)
-    #print("***DIAG: request.args['username']  ***") this pair doesnt work because the variable "username" probably hasnt been defined yet.
-    #print(request.args['username'])
-    #print("***DIAG: request.headers ***")
-    #print(request.headers)
-    return render_template( 'login.html' )
-
-
-@app.route("/auth", methods=['GET', 'POST'])
-def authenticate():
-    #print("\ntwo\n\n\n")
-    #print("***DIAG: this Flask obj ***")
-    #print(app)
-    #print("***DIAG: request obj ***")
-    #print(request)
-    #print("***DIAG: request.args ***")
-    #print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
-    #print("***DIAG: request.headers ***")
-    #print(request.headers)
-    if request.method == 'POST':
-        u = request.form['username']
-    elif (request.method == 'GET'): u = request.args['username']
+@app.route('/', methods = ['GET', 'POST'])
+def login():
+    print('!!!')
+    if 'username' in session:
+        print('!!')
+        return redirect('/auth')
+    else:
+        return render_template("login.html")
     
-    
-    S['password'] = request.cookies.get('password')
-    S['KEY'] = app.secret_key
-    return render_template( 'response.html', user = u, form = request.method)  #response to a form submission
+@app.route('/auth', methods = ['GET', 'POST'])
+def auth_login():
+    if 'username' in session:
+        return render_template("response.html", user = session['username'])
+    if (request.method == 'POST'): login = request.form
+    else: login = request.args
+    print(login)
+    username = login['username']
+    password = login['password']
+    session['username'] = username
+    return render_template("response.html", user = username)
 
-@app.route("/logout", methods=['GET', 'POST'])
-def goodbye():
-    S.pop('password')
-    S.pop('KEY')
-    return render_template( 'logout.html')
+@app.route('/logout', methods = ['GET', 'POST'])
+def logout():
+    del session['username']
+    return redirect('/')
 
-
-    
 if __name__ == "__main__": #false if this file imported as module
-    #enable debugging, auto-restarting of server when this file is modified
-    app.debug = True 
+    app.debug = True
     app.run()
